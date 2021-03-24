08ecdc2cb396bcd6d638c7a65815d193491c6aa4
Lemixu <lemix.x@mail.ru>
2021-03-24 22:40:04 +0300

    Test 1

---
+++ src/test/java/MainClassTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass{
+
+    @Test
+    public void testGetLocalNumber(){
+        Assert.assertEquals("Возвращаемое число не равно 14",14,getLocalNumber());
+
+    }
+}
