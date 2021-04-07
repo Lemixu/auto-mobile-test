c5e4d2bacd1a48de3bbe34d0838a000ee7af61eb
Султанова Алина Маратовна <AMaSultanova@sberbank.ru>
2021-04-07 22:33:58 +0300

    Ex4: Test: Check word in search

--- src/test/java/Ex4.java
+++ src/test/java/Ex4.java
@@ -1,9 +1,46 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class Ex4 extends First{
+import java.util.List;
+import java.util.Locale;
+
+public class Ex4 extends First {
+
+    String inputText = "JAVA";
 
     @Test
-    public void checkWordsInSearch(){
+    public void checkWordsInSearch() {
+        waitForElementAndClick(
+                By.xpath("//androidx.cardview.widget.CardView[@resource-id='org.wikipedia:id/search_container']"),
+                "Cannot find search input",
+                5);
+        waitForElementAndSenKeys(
+                By.xpath("//android.widget.EditText[@resource-id='org.wikipedia:id/search_src_text']"),
+                inputText,
+                "Cannot find search input",
+                5);
+
+        waitForElementPresent(By.id("org.wikipedia:id/page_list_item_title"),
+                "Cannot find articles",
+                15);
+
+        int count = driver.findElements(By.id("org.wikipedia:id/page_list_item_title")).size();
+
+        List<WebElement> element_title = driver.findElementsByXPath(
+                "//android.widget.TextView[contains(@text, '" + firstUpperCase(inputText) + "')]");
+        Assert.assertEquals("There are some articles without input word",
+                count, element_title.size());
+
 
     }
+
+    public String firstUpperCase(String word){
+        if(word == null || word.isEmpty()) return "Input word is empty";
+        word = word.toLowerCase(Locale.ROOT);
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+
+
 }
